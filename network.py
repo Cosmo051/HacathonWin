08a4e7fc399bb242a4291e55b222f964046907a5
MiyamuraKun756 <maromdanino@gmail.com>
2024-04-18 15:12:52 +0300

    ip

--- network.py
+++ network.py
@@ -4,7 +4,7 @@ class Network:
     def __init__(self):
         self.client =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = "192.168.164.219" #need to be the same number from the server!!!
-        self.server = "192.168.164.133" #need to be the same number from the server!!!(ariel)
+        #self.server = "192.168.164.133" #need to be the same number from the server!!!(ariel)
         self.port = 5555
         self.addr = (self.server, self.port)
         self.pos = self.connect()
