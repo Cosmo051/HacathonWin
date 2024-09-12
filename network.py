03132a584ff783c7ed59263b55490ee00dc7c130
trh4 <trh4445@gmail.com>
2024-09-12 20:02:39 +0300

    working text for prev

--- network.py
+++ network.py
@@ -1,28 +1,27 @@
 import socket
 
+
 class Network:
     def __init__(self):
-        self.client =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.164.219" #need to be the same number from the server!!!
-        self.server = "192.168.68.133" #need to be the same number from the server!!!(ariel)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "127.0.0.1"  # Change to server IP if needed
         self.port = 5555
         self.addr = (self.server, self.port)
         self.pos = self.connect()
-    
+
     def get_pos(self):
         return self.pos
 
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return self.client.recv(2048*2).decode()
+            return self.client.recv(2048).decode()
         except:
             pass
-    
+
     def send(self, data):
         try:
             self.client.send(str.encode(data))
-            return self.client.recv(2048*2).decode()
+            return self.client.recv(2048).decode()
         except socket.error as e:
             print(e)
-
--- server.py
+++ server.py
@@ -1,101 +1,60 @@
 import socket
 from _thread import *
-import sys
-import random
 
-server = "192.168.164.219"  #ip adress we need to put in
-server = "192.168.68.133"  #ip adress we need to put in (ariel pc)
+server = "127.0.0.1"
 port = 5555
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#blackbox
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#there is a chance that the port 5555 is taken so we need to add try so it will not crush
 try:
     s.bind((server, port))
-except socket.error as e: #if there is an error we will see e
+except socket.error as e:
     str(e)
 
-s.listen(2) #limit for 2 ppl
+s.listen(2)
 print("Waiting for a connection, Server Started")
 
-def read_pos(str:str):
-    if str == "reset":
-        return "reset"
-    str = str.split("_")
-    return int(str[0]), int(str[1]), str[2], eval(str[3]), int(str[4])
+positions = [(50, 50), (700, 500)]  # Starting positions for players
+
+
+def read_pos(pos):
+    pos = pos.split(",")
+    return int(pos[0]), int(pos[1])
+
 
 def make_pos(tup):
-    if tup == "reset":
-        return "reset"
-    return str(tup[0]) + "_" + str(tup[1]) + "_" + str(tup[2]) + "_" + str(tup[3]) + "_" + str(tup[4])
+    return str(tup[0]) + "," + str(tup[1])
 
-cris_pos = [
-    [[
-        random.randint(50, 1550),
-        random.randint(50, 1550),
-        random.randint(50, 1550),
-        random.randint(50, 1550),
-        random.randint(50, 1550)
-    ],[
-        random.randint(100, 630),
-        random.randint(100, 630),
-        random.randint(100, 630),
-        random.randint(100, 630),
-        random.randint(100, 630),
-    ]],
-    [[
-        random.randint(50, 1550),
-        random.randint(50, 1550),
-        random.randint(50, 1550),
-        random.randint(50, 1550),
-        random.randint(50, 1550)
-    ],[
-        random.randint(100, 630),
-        random.randint(100, 630),
-        random.randint(100, 630),
-        random.randint(100, 630),
-        random.randint(100, 630),
-    ]]
-]
-scroll = 0
-pos = [(11,634, "Idle", cris_pos[0], scroll), (11,634, "Idle", cris_pos[1], scroll)]
-reset_pos = [(11,634, "Idle", cris_pos[0], scroll), (11,634, "Idle", cris_pos[1], scroll)]
 
-def threaded_client(conn, current_player):
-    conn.send(str.encode(make_pos(pos[current_player])))
-    reply = ""
+def threaded_client(conn, player):
+    conn.send(str.encode(make_pos(positions[player])))
     while True:
         try:
-            data = read_pos(conn.recv(2048*4).decode()) #if thre is an error increas the size
+            data = read_pos(conn.recv(2048).decode())
 
             if not data:
                 print("Disconnected")
                 break
-            else:
-                if data == "reset":
-                    pos[0] = reset_pos[0]
-                    pos[1] = reset_pos[1]
-                    continue
-                else:
-                    if current_player == 1:
-                        reply = pos[0]
-                    else:
-                        reply = pos[1]
 
-                print("Received: ", data)
-                print("Sending: ", reply)
-            pos[current_player] = data
+            positions[player] = data
+
+            if player == 1:
+                reply = positions[0]
+            else:
+                reply = positions[1]
+
             conn.sendall(str.encode(make_pos(reply)))
         except:
             break
-    
+
     print("Lost connection")
     conn.close()
 
+
 current_player = 0
 while True:
-    conn, addr = s.accept()  #conn = connection addr = addres
-    print("Connected to: addr")
+    conn, addr = s.accept()
+    print("Connected to:", addr)
 
-    start_new_thread (threaded_client, (conn, current_player))
+    start_new_thread(threaded_client, (conn, current_player))
     current_player += 1
